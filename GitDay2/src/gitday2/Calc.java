a08331e94ca97f9bd7ff539b69e6e52a1dd264ae
Mazen <eng.mazen96@gmail.com>
2020-02-13 17:03:19 +0200

    add .ignor file

---
+++ GitDay2/src/gitday2/Calc.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitday2;
+
+/**
+ *
+ * @author Mazen Mohamed
+ */
+public class Calc implements Calculator{
+    /*start Mazen */
+    @Override
+    public double sum(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double sub(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /*end mazen*/
+    
+    /*start Ahmed*/
+    @Override
+    public double multi(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double div(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    /*end Ahmed*/
+}
